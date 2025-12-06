76fe9bed426bf9eea411f42e5f301a9003893fb8
Tai Tran <107969594+MobaEnder@users.noreply.github.com>
2025-12-06 21:39:45 +0700

    Update lich.js

--- commands/lich.js
+++ commands/lich.js
@@ -2,19 +2,27 @@ const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('lich')
-        .setDescription('Gửi link Kuva Lich / kênh Discord liên quan'),
+        .setName('lich_sister')
+        .setDescription('Gửi link Kuva Lich / Sisters of Parvos / Discord guide'),
 
     async execute(interaction) {
 
         const embed = new EmbedBuilder()
-            .setTitle("🔗 Kuva Lich – Link & Hướng Dẫn")
+            .setTitle("🔗 Kuva Lich – Sisters of Parvos Links & Guides")
             .setColor(0xC00000)
             .setDescription(
-                "**Wiki Kuva Lich:**\n" +
-                "[Nhấn vào đây](https://warframe.fandom.com/wiki/Kuva_Lich)\n\n" +
-                "**Kênh thảo luận trên Discord:**\n" +
-                "[Vào kênh tại đây](https://discord.com/channels/1240686737332768862/1401272735530877008)"
+                "**🎬 Video Tiếng Việt:**\n" +
+                "[YouTube](https://www.youtube.com/watch?v=z_EgUKwOm7w)\n\n" +
+
+                "**📚 Wiki Kuva Lich:**\n" +
+                "[Kuva Lich Wiki](https://wiki.warframe.com/w/Kuva_Lich)\n\n" +
+
+                "**📚 Wiki Sisters of Parvos:**\n" +
+                "[Sisters of Parvos Wiki](https://wiki.warframe.com/w/Sisters_of_Parvos)\n\n" +
+
+                "**💬 Guide Từ Discord:**\n" +
+                "[Kuva Lich Guide](https://discord.com/channels/1240686737332768862/1401272735530877008)\n" +
+                "[Sisters of Parvos Guide](https://discord.com/channels/1240686737332768862/1405195413056917544)"
             );
 
         await interaction.reply({ embeds: [embed] });
