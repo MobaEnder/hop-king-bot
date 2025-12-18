8d11987a4b6c9a29b4f6dc3e04266a2dc4eeaa34
Tai Tran <107969594+MobaEnder@users.noreply.github.com>
2025-12-18 19:31:59 +0700

    Update matcher.js

--- riven/matcher.js
+++ riven/matcher.js
@@ -1,21 +1,23 @@
 module.exports.matchRiven = (auction, config) => {
   const item = auction.item;
 
-  if (!item.weapon_url_name.includes(config.weapon.toLowerCase()))
-    return false;
+  if (!item || !item.weapon_url_name) return false;
+  if (item.weapon_url_name !== config.weapon) return false;
 
   const positives = item.attributes
     .filter(a => a.positive)
     .map(a => a.url_name);
 
-  const negative = item.attributes.find(a => !a.positive)?.url_name || "";
+  const negative =
+    item.attributes.find(a => !a.positive)?.url_name || "";
 
   for (const stat of config.stats) {
     if (!positives.includes(stat)) return false;
   }
 
-  if (config.negative && !negative.includes(config.negative))
+  if (config.negative && !negative.includes(config.negative)) {
     return false;
+  }
 
   return true;
 };
