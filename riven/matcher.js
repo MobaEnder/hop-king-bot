e41eb37af5fa9ea75ab16bd9fdfcbd02c105dd1f
Tai Tran <107969594+MobaEnder@users.noreply.github.com>
2025-12-18 19:14:25 +0700

    Update matcher.js

--- riven/matcher.js
+++ riven/matcher.js
@@ -1,10 +1,21 @@
-const fetch = require('node-fetch');
+module.exports.matchRiven = (auction, config) => {
+  const item = auction.item;
 
-module.exports.getAuctions = async () => {
-  const res = await fetch(
-    "https://api.warframe.market/v1/auctions/search?type=riven&status=open"
-  );
-  const json = await res.json();
-  return json.payload.auctions;
+  if (!item.weapon_url_name.includes(config.weapon.toLowerCase()))
+    return false;
+
+  const positives = item.attributes
+    .filter(a => a.positive)
+    .map(a => a.url_name);
+
+  const negative = item.attributes.find(a => !a.positive)?.url_name || "";
+
+  for (const stat of config.stats) {
+    if (!positives.includes(stat)) return false;
+  }
+
+  if (config.negative && !negative.includes(config.negative))
+    return false;
+
+  return true;
 };
-
