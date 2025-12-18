5eec0c0855fb0ceaf11c3efe5a946128ccdcfa0f
Tai Tran <107969594+MobaEnder@users.noreply.github.com>
2025-12-18 19:14:08 +0700

    Update matcher.js

--- riven/matcher.js
+++ riven/matcher.js
@@ -1,1 +1,10 @@
+const fetch = require('node-fetch');
+
+module.exports.getAuctions = async () => {
+  const res = await fetch(
+    "https://api.warframe.market/v1/auctions/search?type=riven&status=open"
+  );
+  const json = await res.json();
+  return json.payload.auctions;
+};
 
