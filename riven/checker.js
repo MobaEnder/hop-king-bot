39d27a84795800bcc91907d0bae88cfe189d9dfe
Tai Tran <107969594+MobaEnder@users.noreply.github.com>
2025-12-18 19:15:02 +0700

    Update checker.js

--- riven/checker.js
+++ riven/checker.js
@@ -1,1 +1,25 @@
+const { getAuctions } = require('./fetcher');
+const { matchRiven } = require('./matcher');
+const { notify } = require('./notifier');
+const config = require('./config.json');
 
+const seen = new Set();
+
+module.exports = function startRivenChecker(client) {
+  setInterval(async () => {
+    try {
+      const auctions = await getAuctions();
+
+      for (const auction of auctions) {
+        if (seen.has(auction.id)) continue;
+
+        if (matchRiven(auction, config)) {
+          await notify(client, auction);
+          seen.add(auction.id);
+        }
+      }
+    } catch (err) {
+      console.error("Riven checker error:", err.message);
+    }
+  }, 5000);
+};
